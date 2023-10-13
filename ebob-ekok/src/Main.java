7bd740c407c9926d21a3376c7bee520933c7ed39
dogukaan1 <dgkaan67@gmail.com>
2023-10-13 03:15:26 +0300

    ebobEkok
    ebobEkok

---
+++ ebob-ekok/src/Main.java
@@ -0,0 +1,32 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1,n2,i=1,ebob=1,ekok=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the smaller number: ");
+        n1 = input.nextInt();
+        System.out.print("Enter the larger number: ");
+        n2 = input.nextInt();
+
+        while(i <= n1){
+            if(n1%i==0 && n2%i==0){
+                ebob = i;
+            }
+            i++;
+        }
+        System.out.println("Ebob: " + ebob);
+
+
+        while(i<=n1*n2){
+            if(i%n1==0 && i%n2==0){
+                ekok = i;
+                break;
+            }
+            i++;
+        }
+        System.out.print("Ekok: " + ekok);
+    }
+}
